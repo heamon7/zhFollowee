b1f807d6d5f92dc934ae827016ab995c85b64e2f
heamon7 <heamon7@gmail.com>
2015-06-14 23:22:36 +0800

    add content-type header

--- zhFollowee/settings.py
+++ zhFollowee/settings.py
@@ -26,8 +26,10 @@ DEFAULT_REQUEST_HEADERS = {
            'Accept-Encoding': 'gzip, deflate, sdch',
            'Accept-Language': 'zh-CN,zh;q=0.8,en-US;q=0.6,en;q=0.4,zh-TW;q=0.2',
            'Connection': 'keep-alive',
+           'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
            'Host': 'www.zhihu.com',
            'Referer': 'http://www.zhihu.com/',
+           'X-Requested-With': 'XMLHttpRequest',
 
 }
 
