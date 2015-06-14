a8d3f0829d6484fa134f7ca988dff74bcbfdd0fd
heamon7 <heamon7@gmail.com>
2015-06-14 23:36:18 +0800

    add content-type header

--- zhFollowee/settings.py
+++ zhFollowee/settings.py
@@ -21,17 +21,28 @@ ITEM_PIPELINES = {
    # 'zhihut.pipelines.SecondPipline': 800,
 }
 
+
 DEFAULT_REQUEST_HEADERS = {
            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
            'Accept-Encoding': 'gzip, deflate, sdch',
            'Accept-Language': 'zh-CN,zh;q=0.8,en-US;q=0.6,en;q=0.4,zh-TW;q=0.2',
            'Connection': 'keep-alive',
-           'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
            'Host': 'www.zhihu.com',
            'Referer': 'http://www.zhihu.com/',
-           'X-Requested-With': 'XMLHttpRequest',
 
 }
+#
+# DEFAULT_REQUEST_HEADERS = {
+#            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+#            'Accept-Encoding': 'gzip, deflate, sdch',
+#            'Accept-Language': 'zh-CN,zh;q=0.8,en-US;q=0.6,en;q=0.4,zh-TW;q=0.2',
+#            'Connection': 'keep-alive',
+#            'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+#            'Host': 'www.zhihu.com',
+#            'Referer': 'http://www.zhihu.com/',
+#            'X-Requested-With': 'XMLHttpRequest',
+#
+# }
 
 USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36'
 
